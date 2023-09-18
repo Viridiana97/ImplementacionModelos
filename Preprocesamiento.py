0829d086e1c3d857f695a7c9478bf89519f67ac0
Viridiana97 <Viridiana97>
2023-09-18 14:39:57 +0000

    proceso concluido

--- Preprocesamiento.py
+++ Preprocesamiento.py
@@ -1,2 +1,18 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC Carga de datos
 
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC Limpieza
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Preprocesamiento
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Entrenamiento
